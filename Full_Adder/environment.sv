a48ea7c24b6e1e1c6e38da50ecc8028b546c125a
N S Devika <nsdevika2000@gmail.com>
2026-04-20 13:53:14 +0530

    Create environment.sv

---
+++ Full_Adder/environment.sv
@@ -0,0 +1,36 @@
+`include "Transaction.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
+
+class environment;
+  Transaction trans;
+  generator gen;
+  driver drv;
+  monitor mon;
+  scoreboard scb;
+  
+  mailbox gen2drv;
+  mailbox mon2scb;
+  virtual intf vif;
+  
+  function new(virtual intf vif);  
+    this.vif=vif;
+    gen2drv=new();
+    mon2scb=new();  
+    gen=new(gen2drv);
+    drv=new(vif,gen2drv);   
+    mon=new(vif,mon2scb);
+    scb=new(mon2scb);  
+  endfunction
+  
+  task test_run();
+    fork
+      gen.main();
+      drv.main();
+      mon.main();
+      scb.main();
+    join
+  endtask
+endclass
