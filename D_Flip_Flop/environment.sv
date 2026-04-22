707229ae223bf53260f56c50cb85dec4aee6c6f9
N S Devika <nsdevika2000@gmail.com>
2026-04-22 14:09:42 +0530

    Add environment class for D Flip Flop simulation

---
+++ D_Flip_Flop/environment.sv
@@ -0,0 +1,41 @@
+`include "transaction.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
+
+class environment;
+  transaction trans;
+  generator gen;
+  driver drv;
+  monitor mon;
+  scoreboard scb;
+  
+  mailbox gen2drv;
+  mailbox mon2scb;
+  virtual intf vif;
+  event triggered; 
+ 
+  function new(virtual intf vif);  
+    this.vif=vif;
+    
+    gen2drv=new();
+    mon2scb=new();  
+    gen=new(gen2drv);
+    drv=new(vif,gen2drv);   
+    mon=new(vif,mon2scb);
+    scb=new(mon2scb); 
+    
+    gen.ended=this.triggered;
+    scb.trigr=this.triggered;
+  endfunction
+  
+  task test_run();
+    fork
+      gen.main();
+      drv.main();
+      mon.main();
+      scb.main();
+    join
+  endtask
+endclass
