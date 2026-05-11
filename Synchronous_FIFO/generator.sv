c9bb1a66940c1d5e8da5616f93d2a4b4175793b7
N S Devika <nsdevika2000@gmail.com>
2026-05-11 14:40:25 +0530

    Add generator class for transaction generation

---
+++ Synchronous_FIFO/generator.sv
@@ -0,0 +1,21 @@
+class generator#(parameter DEPTH=8, DATA_WIDTH=8);
+  transaction trans;
+  mailbox gen2drv;
+  event ended;
+ 
+  function new(mailbox gen2drv);
+    this.gen2drv=gen2drv;
+  endfunction
+ 
+  task main();
+    repeat(20)
+        begin
+          trans=new();
+          void'(trans.randomize());
+          gen2drv.put(trans);
+          trans.display("Generator class signals");
+          @(ended);
+        end
+  endtask
+ 
+endclass
